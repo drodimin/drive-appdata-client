9af025cc176fc62c097f4b0bc406cd25b294e314
drodimin <49958755+drodimin@users.noreply.github.com>
2020-09-02 10:11:49 -0400

    initial commit

---
+++ client.js
@@ -0,0 +1,89 @@
+const {google} = require('googleapis');
+
+module.exports = class Client {   
+    constructor(authClient, verbose=false){``
+        this._verbose = verbose;
+        this._drive = google.drive({version: 'v3', auth: authClient});
+    }
+    
+    log(...args){
+        if(this._verbose){
+            console.log(...args);
+        }
+    }
+
+    find(filename){
+        this.log("Find file", filename);
+        return new Promise((resolve, reject) => {    
+            try{       
+                this._drive.files.list({
+                    q: "name='" + filename + "'",
+                    spaces: 'appDataFolder',
+                    fields: 'nextPageToken, files(id, name)',
+                }).then(result => {
+                    resolve(result.data.files);
+                })
+                .catch(error => {
+                    reject(error)});
+            } catch(error) {
+                reject(error);
+            }
+        });
+    }
+
+    update(fileId, data){
+        this.log("Update file", fileId);
+        return new Promise((resolve, reject) => {
+            try{
+                this._drive.files.update({
+                    fileId,
+                    media: {
+                        mimeType: 'application/json',
+                        body: data
+                    }})
+                    .then(file => resolve(file.data))
+                    .catch(error => reject(error));
+            } catch(error) {
+                reject(error);
+            }
+        }); 
+    }
+
+    create(fileName, data)
+    {
+        this.log("Update file", fileName);
+        return new Promise((resolve, reject) => {
+            var fileMetadata = {
+                'name': fileName,
+                'parents': ['appDataFolder']
+            };
+            var media = {
+                mimeType: 'application/json',
+                body: data
+            };
+            try{
+                this._drive.files.create({
+                        resource: fileMetadata,
+                        media: media,
+                        fields: 'id'
+                    }).then(file => resolve(file.data))
+                    .catch(error => reject(error));
+            } catch (error) {
+                reject(error);
+            }                
+        });
+    }
+
+    get(fileId){
+        console.log("Getting file", fileId);
+        return new Promise((resolve, reject) => {
+            try{
+                this._drive.files.get({fileId, alt: 'media'})
+                .then(file => resolve(file.data))
+                .catch(error => reject(error));
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
+}
---
+++ client.test.js
@@ -0,0 +1,149 @@
+const mockList = jest.fn();
+const mockUpdate = jest.fn();
+const mockCreate = jest.fn();
+const mockGet = jest.fn();
+
+const driveFiles = {files:{
+    list:mockList,
+    update:mockUpdate,
+    create:mockCreate,
+    get:mockGet}}
+
+const mockDrive = jest.fn(()=> driveFiles);
+
+const jsonData = '{"test":"1"}';
+const filename = 'data.json';
+const error = "test error";
+
+jest.mock('googleapis', () =>({
+    google: {
+        drive: mockDrive
+    }
+  }));
+
+const Client = require('./client');
+
+describe('find', ()=>{    
+    it('resolved to data.files property of drive.files.list result', async() => {
+        //arrange
+        const foundFiles = ["test"];
+        const result = {data:{files:foundFiles}};
+        mockList.mockImplementation((a)=>Promise.resolve(result));
+        const client = new Client({});
+    
+        //act, assert
+        await expect(client.find("test")).resolves.toEqual(foundFiles);
+    })
+
+    it('rejects when drive.files.list rejects', async() => {
+        //arrange
+        mockList.mockImplementation((a)=>Promise.reject(error));
+        const client = new Client({});
+    
+        //act, assert
+        await expect(client.find("test")).rejects.toEqual(error);
+    })
+
+    it('rejects when drive.files.list throws error', async() => {
+        //arrange
+        mockList.mockImplementation((a)=>{ throw error; });
+        const client = new Client({});
+    
+        //act, assert
+        await expect(client.find("test")).rejects.toEqual(error);
+    })
+});
+
+describe('update', ()=>{    
+    const data = '"test":"1"';
+    it('resolves to data property of the drive.files.update result', async() => {
+        //arrange
+        const result = {id:1};
+        mockUpdate.mockImplementation((a)=>Promise.resolve({data:result}));
+        const client = new Client({});
+    
+        //act, assert
+        await expect(client.update(filename, data)).resolves.toEqual(result);
+    })
+
+    it('rejects when drive.files.update rejects', async() => {
+        //arrange
+        const result = undefined;
+        const error = "test error";
+        mockUpdate.mockImplementation((a)=>Promise.reject(error));
+        const client = new Client({});
+    
+        //act, assert
+        await expect(client.update(filename, data)).rejects.toEqual(error);
+    })
+
+    it('rejects when drive.files.update throws error', async() => {
+        //arrange
+        const result = undefined;
+        const error = "test error";
+        mockUpdate.mockImplementation((a)=> { throw error;});
+        const client = new Client({});
+    
+        //act, assert
+        await expect(client.update(filename, data)).rejects.toEqual(error);
+    })
+});
+
+describe('create', ()=>{        
+    it('resolves to data property of drive.files.create result', async() => {
+        //arrange
+        const result = {id:1};
+        mockCreate.mockImplementation((a)=>Promise.resolve({data:result}));
+        const client = new Client({});
+    
+        //act, assert
+        await expect(client.create(filename, jsonData)).resolves.toEqual(result);
+    })
+
+    it('rejects when drive.files.create rejects', async() => {
+        //arrange
+        mockCreate.mockImplementation((a)=>Promise.reject(error));
+        const client = new Client({});
+    
+        //act, assert
+        await expect(client.create(filename, jsonData)).rejects.toEqual(error);
+    })
+
+    it('rejects when drive.files.create throws error', async() => {
+        //arrange
+        mockCreate.mockImplementation((a)=>{ throw error; });
+        const client = new Client({});
+    
+        //act, assert
+        await expect(client.create(filename, jsonData)).rejects.toEqual(error);
+    })
+});
+
+describe('get', ()=>{ 
+    const client = new Client({});
+    const fileId = '1';
+    
+    it('resolves to data property of drive.files.get result', async() => {
+        //arrange
+        mockGet.mockImplementation((a)=>Promise.resolve({data:jsonData}));
+    
+        //act, assert
+        await expect(client.get(fileId)).resolves.toEqual(jsonData);
+    })
+
+    it('rejects when drive.files.get rejects', async() => {
+        //arrange
+        mockGet.mockImplementation((a)=>Promise.reject(error));
+    
+        //act, assert
+        await expect(client.get(fileId)).rejects.toEqual(error);
+    })
+
+    it('rejects when drive.files.get throws error', async() => {
+        //arrange
+        mockGet.mockImplementation((a)=>{ throw error; });
+    
+        //act, assert
+        await expect(client.get(fileId)).rejects.toEqual(error);
+    })
+});
